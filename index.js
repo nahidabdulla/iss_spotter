c1f0f600e9f14a697115bfee49ea384b3623f10c
Nahid <n.abdulla1@hotmail.ca>
2022-10-31 04:01:40 +0000

    create functions nextISSTimesForMyLocation and printPassTimes

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  printPassTimes(passTimes);
+});
+
 
 // const exampleCoords = {latitude: '53.544389', longitude: '-113.4909267'};
 
